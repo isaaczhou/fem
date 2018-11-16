e1245b60a154923ce726ff8979068b7cf92e4da6
isaaczhou <isaaczhou85@gmail.com>
2018-11-16 16:03:01 -0500

    updated

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,8 @@
 import React from "react";
 import pf from "petfinder-client";
+import { Consumer } from "./SearchContext";
 import Pet from "./Pet";
+import SearchBox from "./SearchBox";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -15,8 +17,16 @@ class Results extends React.Component {
     };
   }
   componentDidMount() {
+    this.search();
+  }
+  search = () => {
     petfinder.pet
-      .find({ output: "full", location: "New York, NY" })
+      .find({
+        output: "full",
+        location: this.props.searchParams.location,
+        animal: this.props.searchParams.animal,
+        breed: this.props.searchParams.breed
+      })
       .then(data => {
         let pets;
         if (data.petfinder.pets && data.petfinder.pets.pet) {
@@ -25,14 +35,17 @@ class Results extends React.Component {
           } else {
             pets = [data.petfinder.pets.pet];
           }
+        } else {
+          pets = [];
         }
         this.setState({ pets });
       });
-  }
+  };
 
   render() {
     return (
       <div className="search">
+        <SearchBox search={this.search} />
         {/* <pre>
           <code>{JSON.stringify(this.state, null, 4)}</code>
         </pre> */}
@@ -60,4 +73,10 @@ class Results extends React.Component {
   }
 }
 
-export default Results;
+export default function ResultsWithContext(props) {
+  return (
+    <Consumer>
+      {context => <Results {...props} searchParams={context} />}
+    </Consumer>
+  );
+}
